9cb3acf4a869081b0ccfa3e9f9a1463b7c1ac427
Blake <bryantb2@my.lanecc.edu>
2020-07-22 15:24:02 -0700

    changed file serving system to send static files instead of sending a JSON object

--- routes/index.js
+++ routes/index.js
@@ -1,16 +1,12 @@
 var express = require('express');
 var router = express.Router();
+var path = require('path');
 
-router.get('/updates', (req, res) => {
-  const testUpdates = {
-    "win32-x64-prod": {
-      "readme": "Test release",
-      "update": "https://github.com/djdarkbeat/modapi-react/releases/download/win32-x64-prod-v1.0.1",
-      "install": "https://github.com/djdarkbeat/modapi-react/releases/tag/win32-x64-prod-v1.0.1/ModLEDControl-Setup.exe",
-      "version": "1.0.1"
-    }
-  };
-  res.json(testUpdates);
+router.get('/release/:fileName', (req, res) => {
+  const fileToSend = req.fileName;
+  res.sendFile(`public/release/${fileToSend}`,{
+    root: path.resolve()
+  });
 });
 
 module.exports = router;
